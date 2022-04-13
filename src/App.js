8eb95457617a5b51bad0101e90c80d447cf43d9b
Tony Linz <tonyllinz@gmail.com>
2022-04-13 17:53:16 -0500

    add conditional rendering based on nav selection. add form validation

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ContactForm from "./components/Contact";
 // import "./App.css";
 
 function App() {
+    const [contactSelected, setContactSelected] = useState(false);
     const [categories] = useState([
         {
             name: "commercial",
@@ -29,11 +30,18 @@ function App() {
                 categories={categories}
                 setCurrentCategory={setCurrentCategory}
                 currentCategory={currentCategory}
+                contactSelected={contactSelected}
+                setContactSelected={setContactSelected}
             ></Nav>
             <main>
-                <ContactForm></ContactForm>
-                <Gallery currentCategory={currentCategory}></Gallery>
-                <About></About>
+                {!contactSelected ? (
+                    <>
+                        <Gallery currentCategory={currentCategory}></Gallery>
+                        <About></About>
+                    </>
+                ) : (
+                    <ContactForm></ContactForm>
+                )}
             </main>
         </div>
     );
